e75de6c5de398bb092ae4476fa79a33f2804d009
Juan Manuel <juanpix2404@gmail.com>
2021-04-04 16:13:31 -0500

    Se añade estilo al componente card

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,10 +1,46 @@
 <template>
+  <div class="card">
+    <a href="#" target="_BLANK">
+      <img src="https://place-hold.it/150x250" alt="Anime poster" />
+      <h2>Naruto</h2>
+    </a>
+  </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
 
 <style scoped lang="scss">
+.card {
+  flex: 1 1 33%;
+  width: 33%;
+  max-width: 33%;
+  padding: 0px 8px;
+  margin-bottom: 16px;
+
+  img {
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+    border-radius: 16px;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+    transition: 0.4s;
+  }
+
+  h2 {
+    padding: 16px;
+    color: #313131;
+    transition: 0.4s;
+  }
+
+  &:hover {
+    img {
+      transform: scale(1.05);
+    }
+    h2 {
+      color: #4e0b9b;
+    }
+  }
+}
 </style>
